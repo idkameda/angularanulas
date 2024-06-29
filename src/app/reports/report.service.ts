fe7cbdf2cb0b824d15350dc1cf9d318a1d665039
idkameda <140816929+idkameda@users.noreply.github.com>
2024-06-29 15:48:49 +0530

    Update report.service.ts

--- src/app/reports/report.service.ts
+++ src/app/reports/report.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from "@angular/core";
 import { IReport, IMonthReport } from "./report";
 import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
-import { Observable, throwError, catchError } from "rxjs"; 
+import { Observable, throwError, catchError,of } from "rxjs"; 
 import { map, tap } from 'rxjs/operators';
+import { Data } from "@angular/router";
 
 @Injectable({
     providedIn:'root'
 })
 export class ReportService {
-private reportUrl='http://localhost:88/api/MonthlyReport';
-private reportMonthlyUrl='http://localhost:88/api/MonthlyReportDetails';
+private reportUrl='https://localhost:44316/api/MonthlyReport';
+private reportMonthlyUrl='https://localhost:44316/api/MonthlyReportDetails';
     constructor(private _http: HttpClient) {
 
     }
@@ -22,14 +23,13 @@ private reportMonthlyUrl='http://localhost:88/api/MonthlyReportDetails';
             'Content-Type': 'application/json'  });
         let options = { headers: headers };
 
-        // return this._http.post('http://localhost:88/api/MonthlyReport', data, { headers: headers })
+        // return this._http.post('https://localhost:44316/api/MonthlyReport', data, { headers: headers })
         //     .map((response: Response) => <any[]>response.json());
 
         return this._http.post(this.reportUrl, data, options )
         .pipe(tap((data: any) => console.log(JSON.stringify(data))), catchError(this.handleError))
     }
     async getReportaync(data: any): Promise<any> {
-        debugger
         let headers = new HttpHeaders({
             'Content-Type': 'application/json'  });
         let options = { headers: headers };
@@ -52,10 +52,10 @@ private reportMonthlyUrl='http://localhost:88/api/MonthlyReportDetails';
              'Content-Type': 'application/json'  });
          let options = { headers: headers };
  
-         // return this._http.post('http://localhost:88/api/MonthlyReport', data, { headers: headers })
+         // return this._http.post('https://localhost:44316/api/MonthlyReport', data, { headers: headers })
          //     .map((response: Response) => <any[]>response.json());
  
-         return this._http.post('http://localhost:88/api/MonthlyReportDetails', data, options )
+         return this._http.post('https://localhost:44316/api/MonthlyReportDetails', data, options )
          .pipe(tap((data: any) => console.log(JSON.stringify(data))), catchError(this.handleError))
      }
      async getMonthReportasync(data: any): Promise<any> {
@@ -74,18 +74,44 @@ private reportMonthlyUrl='http://localhost:88/api/MonthlyReportDetails';
       }
       
   getData(data:string){
-    // return this._http.get('http://localhost:88/api/MonthlyReportDetails', 
-    // {
-    //   params: new HttpParams()
-    //   .set('SearchText', data)
-    //   }).pipe(
-    //     map((response:[]) => response.map(item => item['MyDesc']))
-    //   )
+    // return this._http.get(this.reportMonthlyUrl + '?SearchText=' + data)
+    //      .pipe(tap((data: any) => console.log(JSON.stringify(data))), catchError(this.handleError))
+      
+    // return this._http.get('https://localhost:44316/api/MonthlyReportDetails', 
+    //   {
+    //     params: new HttpParams()
+    //     .set('SearchText', data)
+    //     }).pipe(
+    //       map((response:any) => response.map(<Data[]>response.json()))
+    //     )
+    const params = new HttpParams().set('SearchText', data);
+    return this._http.get<any[]>('https://localhost:44316/api/MonthlyReportDetails', { params }).pipe(
+      map(response => response.map(item => item['MyDesc']))
+    );
 
-      return this._http.post('http://localhost:88/api/MonthlyReportDetails', data )
-         .pipe(tap((data: any) => console.log(JSON.stringify(data))), catchError(this.handleError))
   }
 
+//   search(term: string): Observable<any[]> {
+//     return this._http.get(this.reportMonthlyUrl + '?SearchText=' + term)
+//          .pipe(tap((data: any) => console.log(JSON.stringify(data))), catchError(this.handleError))
+// }
+search(term: string): Observable<string[]> {
+  debugger;
+  if (!term.trim()) {
+    return of([]);
+  }
+  return this._http.get<any[]>(`${'https://localhost:44316/api/MonthlyReportDetails'}?SearchText=${term}`).pipe(
+    map(response => response.map(item => item['MyDesc'])),
+    catchError(this.handleError2<string[]>('search', []))
+  );
+}
+
+private handleError2<T>(operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+    console.error(error); // Log the error
+    return of(result as T); // Return an empty result to keep the app running
+  };
+}
   // .map((response: Response) => <any[]>response.json());
     private handleError(err: any) {
         let errorMessage: string;
